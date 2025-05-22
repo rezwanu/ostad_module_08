9898be3bd8afc40e8eef2b01685fed19d39dce2f
Md.Rezwanur Rahman <rezwanureee@gmail.com>
2025-05-22 22:52:14 +0600

    js file update

--- modal/script.js
+++ modal/script.js
@@ -19,6 +19,12 @@ closeModalBtn.addEventListener('click', function (){
     modal.style.display = "none";
 })
 
+window.addEventListener("click", function (event){
+    if(event.target === modal){
+        modal.style.display = "none";
+    }
+})
+
 
 
 })
